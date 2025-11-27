9c99ff17d20206a6d38bcf507233ed80f207f781
Pablo <calmatpablo@gmail.com>
2025-11-27 09:26:03 +0100

    Updated matrices and units (27/11 9:26)

--- Planar_Quadrotor.m
+++ Planar_Quadrotor.m
@@ -1,29 +1,30 @@
 
 %%DEFINE VARIABLES
-u1=1;        % force [N]
-u2=1;        % force [N]
-m=1;         % mass [kg]
-r=1;         % distance 
-I=1;         % moment of inertia []
-g=9.81;      % gravity [m/s^2]
-dt=1;        % increment of time [s]
-theta=1;     % angle [rads]
+u1 = 1;        % force [N]
+u2 = 1;        % force [N]
+m = 1;         % mass [kg]
+r = 1;         % distance [m]
+I = 1;         % moment of inertia [kg*m^2]
+g = 9.81;      % gravity [m/s^2]
+dt = 0.1;        % increment of time [s]
+theta = 1;     % angle [rads]
 
 %%DEFINE MATRIX
-A=[0  1  0  0             0           0,
-   0  0  0  0  -cos(theta)*(u1+u2)/m  0,
-   0  0  0  1             0           0,
-   0  0  0  0  -sin(theta)*(u1+u2)/m  0,
-   0  0  0  0             0           1,
-   0  0  0  0             0           0]
+
+A = [0  1  0  0             0           0,
+     0  0  0  0  -cos(theta)*(u1+u2)/m  0,
+     0  0  0  1             0           0,
+     0  0  0  0  -sin(theta)*(u1+u2)/m  0,
+     0  0  0  0             0           1,
+     0  0  0  0             0           0]
 
 
-F=[1   dt  0   0            0               0,
-   0   1   0   0  -cos(theta)*(u1+u2)*dt/m  0,
-   0   0   1   dt           0               0,
-   0   0   0   1  -sin(theta)*(u1+u2)*dt/m  0,
-   0   0   0   0            1              dt,
-   0   0   0   0            0               1]
+F = [1   dt  0   0            0               0,
+     0   1   0   0  -cos(theta)*(u1+u2)*dt/m  0,
+     0   0   1   dt           0               0,
+     0   0   0   1  -sin(theta)*(u1+u2)*dt/m  0,
+     0   0   0   0            1              dt,
+     0   0   0   0            0               1]
 
 
 B = [0       0,
