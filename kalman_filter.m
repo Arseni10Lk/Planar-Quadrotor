c7a7abdc3f8b8578c3a1edfb609697dfc1a90b3d
Tsimafei <74370156+timofeyilyusenko@users.noreply.github.com>
2025-12-06 14:18:54 +0100

    Update kalman_filter.m

--- kalman_filter.m
+++ kalman_filter.m
@@ -1,34 +1,27 @@
-function [Kalman_prediction] = kalman_filter(A,C,u,time,rotor_data,x0,Ys2)
+function [Pi,Kalman_prediction] = kalman_filter(C,u,rotor_data,state,measurement,P0)
 
 % Quadrotor characteristics
 m = rotor_data.m;
 dt = rotor_data.dt;
 
-% Inititalize output
-Kalman_prediction = zeros(size(A,1),length(time)); % System state by Kalman filter
-
 %Initialize filter
-Kalman_prediction(:,1) = x0;
-R = eye(3)*10^-3; % Measurement noise variance
+R = eye(3)*10^-1; % Measurement noise variance
 Q = eye(6)*10^-3; % Process noise variance
-P0 = eye(6); % Initial variance
 
-for i = 2:length(time)
-
-    % Prediction stage
-    F = eye(6)+dt*[0  1  0  0   0   0;
-                   0  0  0  0   -cos(Kalman_prediction(5,i-1))*(u(i-1,1)+u(i-1,2))/m   0;
-                   0  0  0  1   0   0;
-                   0  0  0  0   -sin(Kalman_prediction(5,i-1))*(u(i-1,1)+u(i-1,2))/m   0;
-                   0  0  0  0   0   1;
-                   0  0  0  0   0   0]; % F matrix depends on the state, so this is the way
-    x_est = F*Kalman_prediction(:,i-1);
-    P_est = F * P0 * F' + Q; % P_i^-
+% Prediction stage
+F = eye(6)+dt*[0  1  0  0   0   0;
+               0  0  0  0   -cos(state(5))*(u(1)+u(2))/m   0;
+               0  0  0  1   0   0;
+               0  0  0  0   -sin(state(5))*(u(1)+u(2))/m   0;
+               0  0  0  0   0   1;
+               0  0  0  0   0   0]; % F matrix depends on the state, so this is the way
+x_est = F*state';
+P_est = F * P0 * F' + Q; % P_i^-
    
-    % Correction stage
+% Correction stage
     
-    k = P_est * C' / (C * P0 * C' + R); % Kalman gain (6 by 3 matrix)
-    Kalman_prediction(:,i) = (eye(6)-k * C) * x_est + k * Ys2(i,:)';
-    P0 = (eye(6) - k * C) * P_est; % Technically iteration stage, but it is united with the last correction step
-end
+k = P_est * C' / (C * P_est * C' + R); % Kalman gain (6 by 3 matrix)
+%Kalman_prediction = (x_est + k * (measurement' - (C * x_est)))';
+Kalman_prediction = (eye(6)-k * C) * x_est + k * measurement';
+Pi = (eye(6) - k * C) * P_est; % Technically iteration stage, but it is united with the last correction step
 end
